1c5fdebb1da7b4a7660a040d71adb7c52940b16e
CahenTaylor <cahentaylor@gmail.com>
2023-06-01 16:44:03 -0600

    asdf

--- server/app.py
+++ server/app.py
@@ -17,7 +17,42 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict_list = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(
+            plants_dict_list,
+            200
+        )
+        return response
+    def post(self):
+        plant_json = request.get_json()
+        new_plant = Plant(
+            name=plant_json.get("name"),
+            image=plant_json.get("image"),
+            price=plant_json.get("price"),
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        plant_dict = new_plant.to_dict()
+        response = make_response(
+            plant_dict,
+            201
+        )
+        return response
+api.add_resource(Plants, '/plants')
+
+class PlantsByID(Resource):
+    def get(self, id):
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+            plant_dict,
+            200
+        )
+        return response
+    
+api.add_resource(PlantsByID, '/plants/<int:id>')
+
+    
 
 class PlantByID(Resource):
     pass
